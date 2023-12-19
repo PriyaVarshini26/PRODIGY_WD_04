f463d99acd19ff2c6bd8a45f44e4bcaa140ed68c
PriyaVarshini26 <154350914+PriyaVarshini26@users.noreply.github.com>
2023-12-19 21:37:02 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,49 @@
+// for home
+function mouseOverh() {
+    document.getElementById("home").style.backgroundColor = "white";
+
+  }
+  function mouseOuth() {
+    document.getElementById("home").style.backgroundColor = "rgb(51,146,153)";
+    
+  }
+// for about
+  function mouseOvera() {
+    document.getElementById("about").style.backgroundColor = "green";
+
+  }
+
+  function mouseOuta() {
+    document.getElementById("about").style.backgroundColor = "rgb(53, 51, 153)";
+    
+  }
+//   for certificate
+  function mouseOverc() {
+    document.getElementById("certificate").style.backgroundColor = "red";
+
+  }
+
+  function mouseOutc() {
+    document.getElementById("certificate").style.backgroundColor = "rgb(153, 51, 119)";
+    
+  }
+//   for internship
+  function mouseOveri() {
+    document.getElementById("internship").style.backgroundColor = "orange";
+
+  }
+
+  function mouseOuti() {
+    document.getElementById("internship").style.backgroundColor = "forestgreen";
+    
+  }
+//   for contact
+  function mouseOvercm() {
+    document.getElementById("contact").style.backgroundColor = "white";
+
+  }
+
+  function mouseOutcm() {
+    document.getElementById("contact").style.backgroundColor = "rgba(255, 0, 17, 0.556)";
+    
+  }
